df456f831774cfe44bcb01df3096e43389e6e4af
Sil van Diepen <me@sil.mt>
2026-07-09 10:23:28 +0200

    perf: lazy load website routes

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,26 +5,18 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue'
-import ChangelogView from './views/ChangelogView.vue'
-import DevelopersView from './views/DevelopersView.vue'
-import DocsView from './views/DocsView.vue'
-import DownloadView from './views/DownloadView.vue'
-import HomeView from './views/HomeView.vue'
-import PluginDetailView from './views/PluginDetailView.vue'
-import PluginsView from './views/PluginsView.vue'
-import PrivacyView from './views/PrivacyView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: HomeView },
-    { path: '/download/', component: DownloadView },
-    { path: '/plugins/', component: PluginsView },
-    { path: '/plugins/:pluginId/', component: PluginDetailView },
-    { path: '/developers/', component: DevelopersView },
-    { path: '/docs/', component: DocsView },
-    { path: '/privacy/', component: PrivacyView },
-    { path: '/changelog/', component: ChangelogView },
+    { path: '/', component: () => import('./views/HomeView.vue') },
+    { path: '/download/', component: () => import('./views/DownloadView.vue') },
+    { path: '/plugins/', component: () => import('./views/PluginsView.vue') },
+    { path: '/plugins/:pluginId/', component: () => import('./views/PluginDetailView.vue') },
+    { path: '/developers/', component: () => import('./views/DevelopersView.vue') },
+    { path: '/docs/', component: () => import('./views/DocsView.vue') },
+    { path: '/privacy/', component: () => import('./views/PrivacyView.vue') },
+    { path: '/changelog/', component: () => import('./views/ChangelogView.vue') },
   ],
   scrollBehavior() {
     return { top: 0 }
